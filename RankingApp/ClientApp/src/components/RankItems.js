2073be06c33eb49f66d0ad66c5f9e28782be80da
akuliakuli <kabby889@gmail.com>
2023-03-02 01:02:47 +0400

    Add project files.

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,56 @@
+﻿import {useEffect } from 'react'
+import MovieImageArr from "./MovieImages.js";
+import RankingGrid from "./RankingGrid";
+import ItemCollection from "./ItemCollection";
+
+
+const RankItems = ({items,setItems,dataType,imgArr,localStorageKey }) => {
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+
+    function drop(ev) {
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+                { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
+        }
+    }
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    }, [dataType])
+
+    useEffect(() => {
+        localStorage.setItem(localStorageKey, JSON.stringify(items));
+
+    },[items])
+
+    return (
+        (items != null) ?
+            <main>
+                <RankingGrid items={items} imgArr={imgArr} drag={drag} allowDrop={allowDrop} drop={drop} />
+                <ItemCollection items={items} drag={drag} imgArr={imgArr} />
+            </main>
+            : <main>Loading...</main>
+    )
+}
+export default RankItems; 
